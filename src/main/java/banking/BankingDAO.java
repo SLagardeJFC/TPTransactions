703129fbd61716707da5eaa3a3349abd2216e589
pedago <pedago@pcalb-mm0609.univ-jfc.fr>
2019-10-15 14:07:34 +0200

    Update + Finish

--- src/main/java/banking/BankingDAO.java
+++ src/main/java/banking/BankingDAO.java
@@ -42,10 +42,19 @@ public class BankingDAO {
 	 * @param amount le montant à trasférer (positif ou nul)
 	 * @throws java.lang.Exception si quelque chose ne marche pas
 	 */
+        
+       /* public boolean customerExists(int client){
+            boolean b = false;
+            
+            return b;
+        }*/
+        
 	public void bankTransferTransaction(int fromID, int toID, float amount) throws Exception {
 		if (amount < 0)
 			throw new IllegalArgumentException("Le montant ne doit pas être négatif");
 	
+                
+                
 		String sql = "UPDATE Account SET Total = Total + ? WHERE CustomerID = ?";
 		try (	Connection myConnection = myDataSource.getConnection();
 			PreparedStatement statement = myConnection.prepareStatement(sql)) {
@@ -56,13 +65,17 @@ public class BankingDAO {
 				statement.setFloat( 1, amount * -1);
 				statement.setInt(2, fromID);
 				int numberUpdated = statement.executeUpdate();
-
+                                if (numberUpdated == 0){
+                                    throw new Exception("Le débité n'existe pas");
+                                }
 				// On crédite le 2° client
 				statement.clearParameters();
 				statement.setFloat( 1, amount);
 				statement.setInt(2, toID);
 				numberUpdated = statement.executeUpdate();
-
+                                if (numberUpdated == 0){
+                                    throw new Exception("Le crédité n'existe pas");
+                                }
 				// Tout s'est bien passé, on peut valider la transaction
 				myConnection.commit();
 			} catch (Exception ex) {
